518350bdb25af1b890ccc11796ea8d98ebabd65f
Bhargava Shastry <bhargava.shastry@ethereum.org>
2022-03-30 11:15:04 +0200

    Yul grammar generator: Bound memory accesses.

--- test/tools/ossfuzz/protoToYul.cpp
+++ test/tools/ossfuzz/protoToYul.cpp
@@ -351,9 +351,22 @@ void ProtoConverter::visit(BinaryOp const& _x)
 		break;
 	}
 	m_output << "(";
-	visit(_x.left());
-	m_output << ",";
-	visit(_x.right());
+	if (op == BinaryOp::KECCAK)
+	{
+		m_output << "mod(";
+		visit(_x.left());
+		m_output << ", " << to_string(s_maxMemory - s_maxSize) << ")";
+		m_output << ",";
+		m_output << "mod(";
+		visit(_x.right());
+		m_output << ", " << to_string(s_maxSize) << ")";
+	}
+	else
+	{
+		visit(_x.left());
+		m_output << ",";
+		visit(_x.right());
+	}
 	m_output << ")";
 }
 
@@ -623,7 +636,14 @@ void ProtoConverter::visit(UnaryOp const& _x)
 		break;
 	}
 	m_output << "(";
-	visit(_x.operand());
+	if (op == UnaryOp::MLOAD)
+	{
+		m_output << "mod(";
+		visit(_x.operand());
+		m_output << ", " << to_string(s_maxMemory) << ")";
+	}
+	else
+		visit(_x.operand());
 	m_output << ")";
 }
 
@@ -775,11 +795,15 @@ void ProtoConverter::visit(CopyFunc const& _x)
 		break;
 	}
 	m_output << "(";
+	m_output << "mod(";
 	visit(_x.target());
+	m_output << ", " << to_string(s_maxMemory - s_maxSize) << ")";
 	m_output << ", ";
 	visit(_x.source());
 	m_output << ", ";
+	m_output << "mod(";
 	visit(_x.size());
+	m_output << ", " << to_string(s_maxSize) << ")";
 	m_output << ")\n";
 }
 
@@ -789,32 +813,42 @@ void ProtoConverter::visit(ExtCodeCopy const& _x)
 	m_output << "(";
 	visit(_x.addr());
 	m_output << ", ";
+	m_output << "mod(";
 	visit(_x.target());
+	m_output << ", " << to_string(s_maxMemory - s_maxSize) << ")";
 	m_output << ", ";
 	visit(_x.source());
 	m_output << ", ";
+	m_output << "mod(";
 	visit(_x.size());
+	m_output << ", " << to_string(s_maxSize) << ")";
 	m_output << ")\n";
 }
 
 void ProtoConverter::visit(LogFunc const& _x)
 {
+	auto visitPosAndSize = [&](LogFunc const& _y) {
+		m_output << "mod(";
+		visit(_y.pos());
+		m_output << ", " << to_string(s_maxMemory - s_maxSize) << ")";
+		m_output << ", ";
+		m_output << "mod(";
+		visit(_y.size());
+		m_output << ", " << to_string(s_maxSize) << ")";
+	};
+
 	switch (_x.num_topics())
 	{
 	case LogFunc::ZERO:
 		m_output << "log0";
 		m_output << "(";
-		visit(_x.pos());
-		m_output << ", ";
-		visit(_x.size());
+		visitPosAndSize(_x);
 		m_output << ")\n";
 		break;
 	case LogFunc::ONE:
 		m_output << "log1";
 		m_output << "(";
-		visit(_x.pos());
-		m_output << ", ";
-		visit(_x.size());
+		visitPosAndSize(_x);
 		m_output << ", ";
 		visit(_x.t1());
 		m_output << ")\n";
@@ -822,9 +856,7 @@ void ProtoConverter::visit(LogFunc const& _x)
 	case LogFunc::TWO:
 		m_output << "log2";
 		m_output << "(";
-		visit(_x.pos());
-		m_output << ", ";
-		visit(_x.size());
+		visitPosAndSize(_x);
 		m_output << ", ";
 		visit(_x.t1());
 		m_output << ", ";
@@ -834,9 +866,7 @@ void ProtoConverter::visit(LogFunc const& _x)
 	case LogFunc::THREE:
 		m_output << "log3";
 		m_output << "(";
-		visit(_x.pos());
-		m_output << ", ";
-		visit(_x.size());
+		visitPosAndSize(_x);
 		m_output << ", ";
 		visit(_x.t1());
 		m_output << ", ";
@@ -848,9 +878,7 @@ void ProtoConverter::visit(LogFunc const& _x)
 	case LogFunc::FOUR:
 		m_output << "log4";
 		m_output << "(";
-		visit(_x.pos());
-		m_output << ", ";
-		visit(_x.size());
+		visitPosAndSize(_x);
 		m_output << ", ";
 		visit(_x.t1());
 		m_output << ", ";
@@ -1012,13 +1040,21 @@ void ProtoConverter::visit(LowLevelCall const& _x)
 		visit(_x.wei());
 		m_output << ", ";
 	}
+	m_output << "mod(";
 	visit(_x.in());
+	m_output << ", " << to_string(s_maxMemory - s_maxSize) << ")";
 	m_output << ", ";
+	m_output << "mod(";
 	visit(_x.insize());
+	m_output << ", " << to_string(s_maxSize) << ")";
 	m_output << ", ";
+	m_output << "mod(";
 	visit(_x.out());
+	m_output << ", " << to_string(s_maxMemory - s_maxSize) << ")";
 	m_output << ", ";
+	m_output << "mod(";
 	visit(_x.outsize());
+	m_output << ", " << to_string(s_maxSize) << ")";
 	m_output << ")";
 }
 
@@ -1045,9 +1081,13 @@ void ProtoConverter::visit(Create const& _x)
 	}
 	visit(_x.wei());
 	m_output << ", ";
+	m_output << "mod(";
 	visit(_x.position());
+	m_output << ", " << to_string(s_maxMemory - s_maxSize) << ")";
 	m_output << ", ";
+	m_output << "mod(";
 	visit(_x.size());
+	m_output << ", " << to_string(s_maxSize) << ")";
 	if (type == Create::CREATE2)
 	{
 		m_output << ", ";
@@ -1066,7 +1106,8 @@ void ProtoConverter::visit(IfStmt const& _x)
 
 void ProtoConverter::visit(StoreFunc const& _x)
 {
-	switch (_x.st())
+	auto storeType = _x.st();
+	switch (storeType)
 	{
 	case StoreFunc::MSTORE:
 		m_output << "mstore(";
@@ -1078,7 +1119,15 @@ void ProtoConverter::visit(StoreFunc const& _x)
 		m_output << "mstore8(";
 		break;
 	}
-	visit(_x.loc());
+	// Write to memory within bounds, storage is unbounded
+	if (storeType == StoreFunc::SSTORE)
+		visit(_x.loc());
+	else
+	{
+		m_output << "mod(";
+		visit(_x.loc());
+		m_output << ", " << to_string(s_maxMemory) << ")";
+	}
 	m_output << ", ";
 	visit(_x.val());
 	m_output << ")\n";
@@ -1259,9 +1308,13 @@ void ProtoConverter::visit(RetRevStmt const& _x)
 		break;
 	}
 	m_output << "(";
+	m_output << "mod(";
 	visit(_x.pos());
+	m_output << ", " << to_string(s_maxMemory - s_maxSize) << ")";
 	m_output << ", ";
+	m_output << "mod(";
 	visit(_x.size());
+	m_output << ", " << to_string(s_maxSize) << ")";
 	m_output << ")\n";
 }
 
@@ -1648,8 +1701,12 @@ void ProtoConverter::fillFunctionCallInput(unsigned _numInParams)
 			m_output << "calldataload(" << slot << ")";
 			break;
 		case 1:
+		{
+			// Access memory within stipulated bounds
+			slot = "mod(" + dictionaryToken() + ", " + to_string(s_maxMemory) + ")";
 			m_output << "mload(" << slot << ")";
 			break;
+		}
 		case 2:
 			m_output << "sload(" << slot << ")";
 			break;
