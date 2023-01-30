5d627994dde44ccdb8a05bc05789d48b819b6115
Matheus Aguiar <matheus.pit@gmail.com>
2023-01-29 22:35:42 -0300

    Add private function to test

--- test/libsolidity/syntaxTests/using/library_functions_attached_in_single_directive_inside_library.sol
+++ test/libsolidity/syntaxTests/using/library_functions_attached_in_single_directive_inside_library.sol
@@ -2,14 +2,18 @@ library L {
     function publicFunction(uint) public pure {}
     function externalFunction(uint) external pure {}
     function internalFunction(uint) internal pure {}
-    using {publicFunction, externalFunction, internalFunction} for uint;
+    function privateFunction(uint) private pure {}
+
+    using {publicFunction, externalFunction, internalFunction, privateFunction} for uint;
+
     function f() public pure {
         uint x;
         x.publicFunction();
         x.externalFunction();
         x.internalFunction();
+        x.privateFunction();
     }
 }
 // ----
-// TypeError 6700: (295-313): Libraries cannot call their own functions externally.
-// TypeError 6700: (323-343): Libraries cannot call their own functions externally.
+// TypeError 6700: (365-383): Libraries cannot call their own functions externally.
+// TypeError 6700: (393-413): Libraries cannot call their own functions externally.
